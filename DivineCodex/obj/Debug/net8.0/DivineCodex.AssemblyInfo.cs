0f56c9fd833e0fd402072118e18b7d489ceaf315
Sameers1 <m.sameersabirbutt@gmail.com>
2024-11-29 07:45:42 +0500

    fixing layout

--- DivineCodex/obj/Debug/net8.0/DivineCodex.AssemblyInfo.cs
+++ DivineCodex/obj/Debug/net8.0/DivineCodex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85463b6ea3a1044293dad327e6244b410d009f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b68878c8cb63dc1fedf798fc4e6cabcd3307dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
