491dae41b0348594f2b4ea8e2f8b47c8d057eab1
Sameer <m.sameersabirbutt@gmail.com>
2024-12-01 12:05:54 +0500

    some more ignore files and now added a admin role with authrize viwe for admin for admin panel.

--- DivineCodex/obj/Debug/net8.0/DivineCodex.AssemblyInfo.cs
+++ DivineCodex/obj/Debug/net8.0/DivineCodex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388c3839cf3c1c8daf733006204225658c0d65d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20fcfa8a1ebd9124881511fee874416e13285d95")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivineCodex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
